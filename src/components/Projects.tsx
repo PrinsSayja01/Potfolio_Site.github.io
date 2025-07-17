bb7ba272bb3700bd35057695c0a58d8a59e74202
PrinsSayja01 <162360493+PrinsSayja01@users.noreply.github.com>
2025-07-17 10:13:46 +0200

    Updated Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -65,7 +65,6 @@ const Projects: React.FC<ProjectsProps> = ({ darkMode }) => {
             </p>
           </div>
 
-          <div className="grid md:grid-cols-2 gap-8">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8">
             {projects.map((project, index) => (
               <div
